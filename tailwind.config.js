ee566cb1f9bdae0234d07ff7939d9cb559513fd8
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2022-09-23 11:50:59 +0700

    baruu

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      'body': ['"Sriracha"', 'cursive']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
